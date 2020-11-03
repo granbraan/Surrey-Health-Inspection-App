4ce84c3d3ec46431c9e6c792a6979c61182a14a0
ritika goyal <rgoyal@sfu.ca>
2020-11-02 16:52:58 -0800

    set date

--- app/src/main/java/com/example/poject_01/model/MyAdapter.java
+++ app/src/main/java/com/example/poject_01/model/MyAdapter.java
@@ -1,19 +1,26 @@
 package com.example.poject_01.model;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.poject_01.R;
+import com.example.poject_01.UI.MainActivity;
+import com.example.poject_01.UI.detail_single_restaurant;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class MyAdapter  extends RecyclerView.Adapter <MyAdapter.MyViewHolder>{
 
@@ -35,7 +42,11 @@ public class MyAdapter  extends RecyclerView.Adapter <MyAdapter.MyViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.date_t.setText("Date - "+inspection_list.getInspectionIndex(position).getInspectionType());
+
+        String   d = "" + inspection_list.getInspectionIndex(position).getInspectionDate();
+        String date = refactorDate(d);
+        holder.date_t.setText(String.format("Date - %s", date));
+
         holder.crit_t.setText("Number of critical issues = "+ inspection_list.getInspectionIndex(position).getNumCritical());
         holder.non_crit_t.setText("Number of non critical issues = "+ inspection_list.getInspectionIndex(position).getNumNonCritical());
         holder.hazard_t.setText("Hazard level = "+inspection_list.getInspectionIndex(position).getHazardRating());
@@ -71,12 +82,32 @@ public class MyAdapter  extends RecyclerView.Adapter <MyAdapter.MyViewHolder>{
         ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            date_t= itemView.findViewById(R.id.date);
+            date_t= itemView.findViewById(R.id.date_inspection);
             crit_t = itemView.findViewById(R.id.num_crit);
             non_crit_t = itemView.findViewById(R.id.num_non_crit);
             hazard_t = itemView.findViewById(R.id.hzrd_lvl);
             image_t = itemView.findViewById(R.id.imageView2);
             mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+
+    private String refactorDate(String d) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate inspectionDate = LocalDate.parse(d, formatter);
+        long difference = DAYS.between(inspectionDate, currentDate);
+
+        Log.d("MainActivity", "current date - "+ currentDate);
+        Log.d("MainActivity", "inspection date - "+ inspectionDate);
+        Log.d("MainActivity", "difference = "+ difference);
+        if (difference <= 30){
+            return( difference + " days ago");
+        }
+        else if(difference <= 365){
+            return("" + inspectionDate.getMonth() + " " + inspectionDate.getDayOfMonth() );
+        }
+        else {
+            return("" + inspectionDate.getYear() + " "+ inspectionDate.getMonth() );
         }
     }
 }
